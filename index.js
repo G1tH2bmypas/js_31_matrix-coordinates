2ad23938deb6e1d6ba543266ec73cfdc2d8f6243
Tekyto <Tekyto2014@gmail.com>
2022-11-23 21:01:50 +0200

    js_31_matrix-coordinates++

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 function getCoordinates(matrix) {
 	let total = {x:0 , y:0};
-	let swith = false;
 
 	for (let i = 0; i < matrix.length; i++) {
-		if (swith) {
-			break;
-		}
 		for (let j = 0; j < matrix[i].length; j++) {
 			if (matrix[i][j] === 1) {
 				total.x = j;
